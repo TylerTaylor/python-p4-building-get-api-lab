a41cedbc7b7866465899c538b26159dbd2b77856
TylerTaylor <vikingtyty@gmail.com>
2023-01-04 11:16:22 -0600

    Done and passing

--- server/app.py
+++ server/app.py
@@ -20,19 +20,38 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakeries.append(bakery.to_dict())
+    response = make_response(bakeries, 200)
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id = id).first()
+    bakery_serialized = bakery.to_dict()
+
+    response = make_response(bakery_serialized, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    bakeries_sorted = []
+    for bakery in BakedGood.query.order_by(BakedGood.price).all():
+        bakeries_sorted.append(bakery.to_dict())
+    response = make_response(bakeries_sorted, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    bg_serialized = baked_good.to_dict()
+
+    response = make_response(bg_serialized, 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
